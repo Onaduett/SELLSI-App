3165b09659b183f3eda2a06dfebec49f66f4c957
dauletanigylator <dauletanigylator@gmail.com>
2025-07-31 14:09:48 +0200

    Initial Commit

---
+++ Ecommerce/EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceApp.swift
+//  Ecommerce
+//
+//  Created by Daulet Yerkinov on 31.07.25.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EcommerceTests/EcommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceTests.swift
+//  EcommerceTests
+//
+//  Created by Daulet Yerkinov on 31.07.25.
+//
+
+import Testing
+@testable import Ecommerce
+
+struct EcommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
